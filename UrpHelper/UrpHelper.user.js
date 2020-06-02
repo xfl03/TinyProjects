a758fe9fc927e327133fe4e0cf3f063ce2d49d93
Alexander Xia <xfl03@hotmail.com>
2020-06-02 19:02:56 +0800

    Update UrpHelper.user.js

--- UrpHelper/UrpHelper.user.js
+++ UrpHelper/UrpHelper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         URP教务系统助手
-// @namespace    http://github.com/xfl03
-// @version      0.1.1
+// @namespace    https://github.com/xfl03
+// @version      0.1.2
 // @description  URP教务系统助手，用于辅助URP教务系统的使用
 // @author       xfl03
 // @match        http://urp.cup.edu.cn/student/teachingEvaluation/teachingEvaluation/evaluationPage
@@ -13,8 +13,16 @@
 
     $(document).ready(function () {
         //解决提交时间限制问题，允许提前提交
-        $("#RemainM").html(0);
-        $("#RemainS").html(1);
+        //修改：已加入后端时间检测，无法提前提交
+        //$("#RemainM").html(0);
+        //$("#RemainS").html(1);
+
+        //提交倒计时结束自动提交
+        var m = parseInt($("#RemainM").html());
+        var s = parseInt($("#RemainS").html());
+        setTimeout(function(){
+            $("#buttonSubmit").click();
+        }, (m * 60 + s) * 1000);
 
         //自动选择最优选项
         var keyWord=["优","很大进步","总是","个别最好的"];
