2d5363614c0990661e7553f6e1d7323009ccd8cd
33 <moe@3-3.dev>
2023-01-01 00:54:00 +0800

    Update: bilibili artical image

--- BilibiliArticalImage/BilibiliArticalImage.user.js
+++ BilibiliArticalImage/BilibiliArticalImage.user.js
@@ -1,37 +1,43 @@
 // ==UserScript==
 // @name         bilibili 专栏高清图片
 // @namespace    https://github.com/xfl03
-// @version      0.1
+// @version      0.2
 // @license      GPL-v3.0
 // @description  在 bilibili 专栏中显示高清图片
 // @author       xfl03
 // @match        https://www.bilibili.com/read/cv*
 // @icon         https://static.hdslb.com/images/favicon.ico
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
+    GM_addStyle(".normal-img:hover{cursor: zoom-in;}");
 
     const imageRegex = /\/\/i0.hdslb.com\/bfs\/article\/(\w+)\.png@(\w+)\.webp/;
-    const targetNode = document.getElementById('article-content');
-    const callback = function(mutationsList, observer) {
-        for(let mutation of mutationsList){
+    const imageFunction = function (image) {
+        const src = image.getAttribute('src');
+        if (!src) return;
+        const m = src.match(imageRegex);
+        if (m) {
+            const newSrc = `//i0.hdslb.com/bfs/article/${m[1]}.png`;
+            image.setAttribute('src', newSrc);
+            image.setAttribute('onclick', `window.open('${newSrc}')`);
+        }
+    }
+
+    const callback = function (mutationsList, observer) {
+        for (let mutation of mutationsList) {
             if (mutation.type === "attributes") {
                 const image = mutation.target;
-
-                const src = image.getAttribute('src');
-                if (!src) continue;
-                const m = src.match(imageRegex);
-                if (m) {
-                    const newSrc = `//i0.hdslb.com/bfs/article/${m[1]}.png`;
-                    //console.log(`图片地址替换: ${newSrc}`);
-                    image.setAttribute('src', newSrc);
-                }
+                imageFunction(image);
             }
         }
     };
+
     //https://developer.mozilla.org/zh-CN/docs/Web/API/MutationObserver/observe
-    const option = {subtree: true, childList: false, attributes: true, attributeFilter: ["src"]};
+    const option = { subtree: true, childList: false, attributes: true, attributeFilter: ["src"] };
+    const targetNode = document.getElementById('article-content');
+    //监听src属性变化
     new MutationObserver(callback).observe(targetNode, option);
 })();
